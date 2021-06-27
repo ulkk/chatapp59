e1f6de7d3285788d54c6146a336ebba4370a7372
ulkk <hideocc90@gmail.com>
2021-06-27 15:48:08 +0900

    LoggedInRouteまで

---
+++ src/pages/Login.js
@@ -0,0 +1,50 @@
+import React , {useState} from 'react'
+import firebase from '../config/firebase'
+
+const Login = () => {
+    const[email,setEmail]=useState('')
+    const[password,setPassword]=useState('')
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        firebase.auth().signInWithEmailAndPassword(email,password)
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <>
+        <h1>Login</h1>
+        <form onSubmit = {handleSubmit}>
+            <div>
+                <label htmlFor = 'email'>E-mail</label>
+                <input
+                    type = 'email'
+                    id = 'email'
+                    name = 'email'
+                    placeholder = 'Email'
+                    onChange={e=>{
+                        setEmail(e.target.value)
+                    }}
+                />
+            </div>
+            <div>
+                <label htmlFor = 'password'>Password</label>
+                <input
+                    type = 'password'
+                    id = 'password'
+                    name = 'password'
+                    placeholder = 'Password'
+                    onChange={e=>{
+                        setPassword(e.target.value)
+                    }}
+                />
+            </div>
+            <button type = 'submit'>Login</button>
+        </form>
+        </>
+    )
+}
+
+export default Login
