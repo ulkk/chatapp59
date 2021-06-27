47f663f75ae7eb111c21f6d42cb44b944a2d265d
kei <xnkcelebrate48x@yahoo.co.jp>
2021-06-27 16:37:40 +0900

    user名を登録できるように

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,49 +1,69 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import firebase from '../config/firebase'
 
-const SignUp = () =>{
-    const[email,setEmail] = useState('')
-    const[password,setPassword] = useState('')
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        firebase.auth().createUserWithEmailAndPassword(email,password)
-        .catch(err => {
-            console.log(err)
+const SignUp = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        user.updateProfile({
+          displayName: name,
         })
-    }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-    return (
-        <>
-        <h1>Sign Up</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor = 'email'>E-mail</label>
-                <input
-                    type = 'email'
-                    id = 'email'
-                    name = 'email'
-                    placeholder = 'Email'
-                    onChange = {e=>{
-                        setEmail(e.target.value)
-                    }}
-                />
-            </div>
-            <div>
-            <label htmlFor = 'password'>Password</label>
-            <input
-                type = 'password'
-                id = 'password'
-                name = 'password'
-                placeholder = 'Password'
-                onChange={e=>{
-                    setPassword(e.target.value)
-                }}
-            />
-            </div>
-            <button type = 'submit'>SignUp</button>
-        </form>
-        </>
-    )
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Name</label>
+          <input
+            type="name"
+            id="name"
+            name="name"
+            placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
+        </div>
+        <button type="submit">SignUp</button>
+      </form>
+    </>
+  )
 }
 
 export default SignUp
