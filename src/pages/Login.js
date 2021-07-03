5796ffbab0b99e7c36c0a640c4e592616880de5a
chimm7 <ckhhaapa7@gmail.com>
2021-07-04 02:13:12 +0900

    Roomコンポーネントの機能を作成

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,60 @@
-import React , {useState} from 'react'
-import firebase from '../config/firebase'
+import React, { useState, useContext } from "react"
+import firebase from "../config/firebase"
+import { AuthContext } from "../AuthService"
+import { Redirect } from "react-router"
 
 const Login = () => {
-    const[email,setEmail]=useState('')
-    const[password,setPassword]=useState('')
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        firebase.auth().signInWithEmailAndPassword(email,password)
-        .catch(err => {
-            console.log(err)
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-    return (
-        <>
-        <h1>Login</h1>
-        <form onSubmit = {handleSubmit}>
-            <div>
-                <label htmlFor = 'email'>E-mail</label>
-                <input
-                    type = 'email'
-                    id = 'email'
-                    name = 'email'
-                    placeholder = 'Email'
-                    onChange={e=>{
-                        setEmail(e.target.value)
-                    }}
-                />
-            </div>
-            <div>
-                <label htmlFor = 'password'>Password</label>
-                <input
-                    type = 'password'
-                    id = 'password'
-                    name = 'password'
-                    placeholder = 'Password'
-                    onChange={e=>{
-                        setPassword(e.target.value)
-                    }}
-                />
-            </div>
-            <button type = 'submit'>Login</button>
-        </form>
-        </>
-    )
+  const user = useContext(AuthContext)
+  if (user) {
+    return <Redirect to="/" />
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">E-mail</label>
+          <input
+            type="name"
+            id="name"
+            name="name"
+            placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
 }
 
 export default Login
