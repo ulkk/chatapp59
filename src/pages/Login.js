d65e25211b5ff839181acc1895b85b919fb5980a
ulkk <hideocc90@gmail.com>
2021-07-04 14:55:01 +0900

    material-uiで見た目を調整

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,147 @@
-import React , {useState} from 'react'
-import firebase from '../config/firebase'
+import React, { useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import { AuthContext } from "../AuthService";
+import firebase from "../config/firebase";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import ChatIcon from "@material-ui/icons/Chat";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const Login = () => {
-    const[email,setEmail]=useState('')
-    const[password,setPassword]=useState('')
-
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        firebase.auth().signInWithEmailAndPassword(email,password)
-        .catch(err => {
-            console.log(err)
-        })
-    }
-
-    return (
-        <>
-        <h1>Login</h1>
-        <form onSubmit = {handleSubmit}>
-            <div>
-                <label htmlFor = 'email'>E-mail</label>
-                <input
-                    type = 'email'
-                    id = 'email'
-                    name = 'email'
-                    placeholder = 'Email'
-                    onChange={e=>{
-                        setEmail(e.target.value)
-                    }}
-                />
-            </div>
-            <div>
-                <label htmlFor = 'password'>Password</label>
-                <input
-                    type = 'password'
-                    id = 'password'
-                    name = 'password'
-                    placeholder = 'Password'
-                    onChange={e=>{
-                        setPassword(e.target.value)
-                    }}
-                />
-            </div>
-            <button type = 'submit'>Login</button>
-        </form>
-        </>
-    )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Login
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login({ history }) {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useContext(AuthContext);
+
+  if (user) {
+    return <Redirect to="/" />;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <ChatIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
