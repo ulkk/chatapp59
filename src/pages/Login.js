30998a31a3424fb5fed18f1d6bf4dc3acb5d6f1e
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2021-07-04 15:30:19 +0900

    修正

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,6 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import ChatIcon from "@material-ui/icons/Chat";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
