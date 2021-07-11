336cf3bb41e13f5652f5eafbe720e612212e924d
chimm7 <ckhhaapa7@gmail.com>
2021-07-11 15:53:00 +0900

    メッセージ曹宇審の際の日時表示

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,58 +1,85 @@
-import { useState, useEffect, useContext } from 'react'
-import { AuthContext } from '../AuthService'
-import firebase from '../config/firebase'
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../AuthService";
+import firebase from "../config/firebase";
+import "firebase/firestore";
 const Room = () => {
-  const [messages, setMessages] = useState([])
-  const [value, setValue] = useState('')
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     firebase
       .firestore()
-      .collection('messages')
+      .collection("messages")
+      .orderBy(`timestamp`)
       .onSnapshot((snapshot) => {
         const messages = snapshot.docs.map((doc) => {
-          return doc.data()
-        })
-        messages.sort(function (a, b) {
-          if (a.date > b.date) {
-            return 1
-          } else if (a.date < b.date) {
-            return -1
-          }
-          return 0
-        })
-        setMessages(messages)
-      })
-  }, [])
+          return {
+            ...doc.data({ serverTimestamps: "estimate" }),
+            timestamp: doc
+              .data({ serverTimestamps: "estimate" })
+              .timestamp.toDate(),
+          };
+          // .timestamp.toDate(),
+          //firebaseからデータ取ってくる。その時にDate型にかえるためのtoDate()を行っている。
+          //({ serverTimestamps: "estimate" }はfirebase.firestore.FieldValue.serverTimestamp() を使うと、すぐにtimestampが入るわけではなく、タイムスタンプが設定されるまでに、少しタイムラグがある。
+          //これを解消するために、timestampがnullの時は見積時間を返すよ、というもの。
+          //Cannot read property 'toDate' of nullの解消
+        });
 
-  const user = useContext(AuthContext)
+        // messages.sort(function (a, b) {
+        //   if (a.date > b.date) {
+        //     return 1
+        //   } else if (a.date < b.date) {
+        //     return -1
+        //   }
+        //   return 0
+        // })
+        setMessages(messages);
+      });
+  }, []);
+
+  const user = useContext(AuthContext);
   const handleSubmit = (e) => {
-    e.preventDefault()
-    firebase.firestore().collection('messages').add({
+    e.preventDefault();
+    firebase.firestore().collection("messages").add({
       content: value,
       user: user.displayName,
-      date: new Date(),
-    })
-  }
-  console.log(user)
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      //サーバー（firestore上）の時間をデータとして追加できる
+    });
+    // date: new Date(),
+  };
+
+  console.log(user);
   return (
     <>
       <h1>Room</h1>
       <ul>
         {messages?.map((message, index) => {
+          const formatTime = `${message.timestamp.getFullYear()}/${
+            message.timestamp.getMonth() + 1
+          }/${message.timestamp.getDate()}/${message.timestamp.getHours()}:${message.timestamp.getMinutes()}:${message.timestamp.getSeconds()}`;
+          //文字列にする
+
           return (
             <li key={index}>
               {message.user}:{message.content}
+              <span>{formatTime}</span>
             </li>
-          )
+          );
         })}
       </ul>
+
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button type="submit">送信</button>
       </form>
       <button onClick={() => firebase.auth().signOut()}>Logout</button>
     </>
-  )
-}
-export default Room
+  );
+};
+export default Room;
