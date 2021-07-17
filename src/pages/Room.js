43166bf41586e2f9d82b7910d8eda6b778e8bc12
takumi <t.takata121@outlook.jp>
2021-07-18 08:19:12 +0900

    途中経過

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -2,11 +2,21 @@ import { useState, useEffect, useContext } from 'react'
 import { AuthContext } from '../AuthService'
 import firebase from '../config/firebase'
 import 'firebase/firestore'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Box from '@material-ui/core/Box'
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 
 const Room = () => {
   const [messages, setMessages] = useState([])
   const [value, setValue] = useState('')
-
+  const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+  }))
   useEffect(() => {
     firebase
       .firestore()
@@ -37,46 +47,61 @@ const Room = () => {
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       //サーバー（firestore上）の時間をデータとして追加できる
     })
-    setValue('')
+    setValue('')//送信後に空にする
   }
 
   console.log(user)
+  const classes = useStyles()
   return (
-    <>
-      <h1>Room</h1>
-      <ul>
-        {messages?.map((message, index) => {
-          const formatTime = `${message.timestamp.getFullYear()}/${
-            message.timestamp.getMonth() + 1
-          }/${message.timestamp.getDate()}/${message.timestamp.getHours()}:${message.timestamp.getMinutes()}:${message.timestamp.getSeconds()}`
-          //文字列にする
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+        <h1>Room</h1>
+        <Box width="75%">
+          {messages?.map((message, index) => {
+            const formatTime = `
+            ${message.timestamp.getHours()}:
+            ${message.timestamp.getMinutes()}`
+            // ${message.timestamp.getFullYear()}/
+            // ${
+              //      message.timestamp.getMonth() + 1
+              //   }/
+              //  ${message.timestamp.getDate()}/
+              //  ${message.timestamp.getSeconds()}
+              //文字列にする
+              
+              return (
+                <>
+                <Box display="flex" flexDirection="row">
+                  <div>
+                    <p>{message.user}:</p>
+                    <p>{message.content}</p>
+                  </div>
+                  <Box display="flex" flexDirection="row" alignItems="flex-end">
+                    <p>{formatTime}</p>
+                    <div
+                      onClick={() => {
+                        if (message.user === user.displayName) {
+                          firebase.firestore().collection('messages').doc(message.id).delete()
+                        }
+                      }}
+                      >
+                      <IconButton aria-label="delete" className={classes.margin} size="small">
+                        <DeleteIcon fontSize="small" />
+                      </IconButton>
+                    </div>
+                  </Box>
+                </Box>
+              </>
+            )
+          })}
+        </Box>
 
-          return (
-            <>
-              <li key={index}>
-                {message.user}:{message.content}:{formatTime}
-                <button
-                  className="style"
-                  onClick={() => {
-                    if (message.user === user.displayName) {
-                      firebase.firestore().collection('messages').doc(message.id).delete()
-                    }
-                  }}
-                >
-                  削除
-                </button>
-              </li>
-            </>
-          )
-        })}
-      </ul>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-        <button type="submit">送信</button>
-      </form>
-      <button onClick={() => firebase.auth().signout()}>Logout</button>
-    </>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+          <button type="submit">送信</button>
+        </form>
+        <button onClick={() => firebase.auth().signout()}>Logout</button>
+    </Container>
   )
 }
 export default Room
